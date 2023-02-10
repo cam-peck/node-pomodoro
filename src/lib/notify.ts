aa7e793f34502d19673f2fa783f7afa306f6f67f
Cameron <cameronpeck317@gmail.com>
2023-02-10 18:10:15 +0000

    fix tomato image path

--- src/lib/notify.ts
+++ src/lib/notify.ts
@@ -1,7 +1,7 @@
 import notifier from 'node-notifier';
 import path from 'path';
 
-const tomatoIcon: string = path.join(__dirname, '../src/images/tomato-icon.png');
+const tomatoIcon: string = path.join(__dirname, '../../src/images/tomato-icon.png');
 
 
 function sendNotification (title: string, message: string) {
